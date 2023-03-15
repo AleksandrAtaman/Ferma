6f91f4da6b9e55ea67967c6d69d07a15f3fdd011
AleksandrAtaman <127784549+AleksandrAtaman@users.noreply.github.com>
2023-03-15 20:10:08 +0300

    ferma

---
+++ .idea/animals.java
@@ -0,0 +1,24 @@
+public class Animals {
+
+    public int weight;
+
+    public String color;
+    public int reproduction;
+    public String production;
+    public String tolk;
+
+    public void productions( int reproduction, String production) {
+        this.reproduction=reproduction;
+        this.production=production;
+    }
+    public void appearance(int weight,String color){
+        this.weight=weight;
+        this.color=color;
+
+    }
+    public String characteristics(){
+        String info=" Вес животного "+ weight+ " Цвет животного "+ color+" Потомство в год =" + reproduction+ " Производит "+ production+" Издает звук "+ tolk;
+        return info;
+    }
+}
+
