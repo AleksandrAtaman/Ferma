9391d4ff7e206825836284cbadebc1b4d1e8f298
AleksandrAtaman <127784549+AleksandrAtaman@users.noreply.github.com>
2023-03-16 18:49:40 +0300

    Ferma

--- .idea/animals.java
+++ .idea/animals.java
@@ -1,11 +1,18 @@
 public class Animals {
 
     public int weight;
-
     public String color;
     public int reproduction;
     public String production;
     public String tolk;
+
+    public void setharacteristics(int weight,String color, int reproduction,String production,String tolk){
+        this.weight=weight;
+        this.color=color;
+        this.reproduction=reproduction;
+        this.production=production;
+        this.tolk=tolk;
+    }
 
     public void productions( int reproduction, String production) {
         this.reproduction=reproduction;
@@ -21,4 +28,5 @@ public class Animals {
         return info;
     }
 }
+
 
--- .idea/ferma.java
+++ .idea/ferma.java
@@ -1,30 +1,34 @@
 public class MyFerm {
     public static void main(String[] args) {
         Animals pig = new Animals();
-        pig.weight = 120;
-        pig.reproduction = 2;
-        pig.production = "Сало";
-        pig.tolk = "Хрю";
-        pig.color = "pink";
-        String res1 = pig.characteristics();
+        pig.setharacteristics(120,"pink",2,"Сало","Хрю");
+
+        String res1=pig.characteristics();
 
         System.out.println(res1);
 
         Animals chiken = new Animals();
-        chiken.weight = 3;
-        chiken.reproduction = 15;
-        chiken.production = "Яйцы";
-        chiken.tolk = "Кух-кудах";
-        chiken.color = "Gray";
-        String res2 = chiken.characteristics();
+        chiken.setharacteristics(3,"Gray",15,"Яйца","Кух-кудах");
+
+//chiken.weight=3;
+//chiken.reproduction=15;
+//chiken.production="Яйцы";
+//chiken.tolk="Кух-кудах";
+//chiken.color="Gray";
+        String res2=chiken.characteristics();
         System.out.println(res2);
 
         Animals cow = new Animals();
-        cow.productions(1, "Мясо и молоко");
-        cow.appearance(280, "Redhead");
-        cow.tolk = "МУУУУУУ";
-        String res3 = cow.characteristics();
+        cow.setharacteristics(280,"RedHead",1,"Мясо и молоко","МУУУУУУ");
+//cow.productions(1,"Мясо и молоко");
+//cow.appearance(280,"Redhead");
+//cow.tolk="МУУУУУУ";
+        String res3=cow.characteristics();
 
         System.out.println(res3);
     }
 }
+
+
+
+
